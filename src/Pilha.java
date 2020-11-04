7770fa8e07a35dbbd4ec21156ab0110da3a309d6
andersonluizpereira <andy2903.alp@gmail.com>
2020-11-04 20:48:34 -0300

    firts test stack null

---
+++ src/Pilha.java
@@ -0,0 +1,14 @@
+
+public class Pilha {
+
+	public boolean estaVazia() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public int tamanho() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
