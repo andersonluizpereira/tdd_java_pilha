16bd098c445d3f5c100ad5bf3600aba5b95cb6e4
andersonluizpereira <andy2903.alp@gmail.com>
2020-11-04 22:20:47 -0300

    should test stack unstack

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,11 +1,16 @@
 
 public class Pilha {
 
-	private Object elemento;
+	private Object[] elementos;
 	private int quantidade = 0;
+	public Pilha(int maximo) {
+		// TODO Auto-generated constructor stub
+		elementos = new Object[maximo];
+	}
+
 	public boolean estaVazia() {
 		// TODO Auto-generated method stub
-		return (this.elemento == null);
+		return this.quantidade == 0;
 	}
 
 	public int tamanho() {
@@ -15,13 +20,20 @@ public class Pilha {
 
 	public void empilha(Object elemento) {
 		// TODO Auto-generated method stub
-		this.elemento = elemento;
+		this.elementos[this.quantidade] = elemento;
 		this.quantidade++;
 	}
 
 	public Object topo() {
 		// TODO Auto-generated method stub
-		return this.elemento;
+		return this.elementos[this.quantidade-1];
+	}
+
+	public Object desempilha() {
+		// TODO Auto-generated method stub
+		Object topo = this.topo();
+		this.quantidade--;
+		return topo;
 	}
 
 }
--- src/TestePilha.java
+++ src/TestePilha.java
@@ -1,22 +1,40 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestePilha {
 
+	private Pilha p;
+	
+	@Before
+	public void incializaPilha() {
+		p = new Pilha(10);
+	}
+	
 	@Test
 	public void pilhaVazia() {
-		Pilha p = new Pilha();
 		assertTrue(p.estaVazia());
 		assertEquals(0, p.tamanho());
 	}
 	
 	@Test
 	public void empilhaUmElemento() {
-		Pilha p = new Pilha();
 		p.empilha("primeiro");
 		assertFalse(p.estaVazia());
 		assertEquals(1, p.tamanho());
 		assertEquals("primeiro", p.topo());
+	}
+	
+	@Test
+	public void empilhaEDesempilha() {
+		p.empilha("primeiro");
+		p.empilha("segundo");
+		assertEquals(2, p.tamanho());
+		assertEquals("segundo", p.topo());
+		Object desempilhado = p.desempilha();
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+		assertEquals("segundo", desempilhado);
 	}
 }
